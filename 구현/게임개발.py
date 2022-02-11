e47873beae6a8f3404e75022869e472810219841
zzzl-523 <zzh23@naver.com>
2022-02-11 23:59:28 +0900

    doing

---
+++ 구현/게임개발.py
@@ -0,0 +1,20 @@
+from posixpath import split
+
+
+sero, garo = map(int, input().spit())
+data = int(input())
+x, y = data[0], data[1]
+nx, ny = x, y
+direction = data[2]
+dx = [0, 1,  0, -1]
+dy = [1, 0, -1, 0]
+
+array = []
+for i in range(sero){
+    array.append(list(map(int, input().split())))    
+}
+
+route = []
+route[x][y] = -1
+while True:
+    if cat == 4:
