b5e524737152fc87dc9e43dad3ef52cec166f695
Jihyun Lee <zzh23@naver.com>
2023-07-13 13:58:29 +0900

    요세푸스 문제-개선

--- 백준 연습/자료구조/요세푸스 문제-1158.py
+++ 백준 연습/자료구조/요세푸스 문제-1158.py
@@ -1,34 +1,41 @@
 from collections import deque
 
 N, K = map(int, input().split())
-ppl_list = [i for i in range(1, N+1)]
+# queue 배열 만들기
+ppl_list = deque([i for i in range(1, N+1)])
 res_list = []
 
 next_num = K
 prev_idx = 0
 prev_num = -1
 
+# 방법1 -> 코드는 복잡하지만 시간이 훨씬 짧음
 for _ in range(N):
     if len(ppl_list) == 1:
         res_list.append(ppl_list[0])
         break
     idx = prev_idx + (K-1)
-    temp_list = ppl_list.copy()
     prev_idx = idx%len(ppl_list)
 
     if idx >= len(ppl_list) - 1:
-        temp_list.extend(ppl_list[:K])
         idx = idx%len(ppl_list)
-        if temp_list[idx] == prev_num:
+        if ppl_list[idx] == prev_num:
             idx += 1
         prev_idx = idx
 
-    num = temp_list[idx]
+    num = ppl_list[idx]
     
     res_list.append(num)
     ppl_list.remove(num)
     prev_num = num
-    
+
+
+# 방법2 -> 코드는 짧지만 시간이 10배 늘어남
+# for _ in range(N):
+#     for _ in range(K-1):
+#         # K번째가 맨 앞에 오도록 그 앞 수들을 가장 뒤로 push하기
+#         ppl_list.append(ppl_list.popleft())
+#     res_list.append(ppl_list.popleft())
     
 result = ''
 for idx, num in enumerate(res_list):
