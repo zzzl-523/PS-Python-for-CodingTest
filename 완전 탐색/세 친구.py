e20cbe547ca8ff03ea6739d87314ce9d3fd13992
Jihyun Lee <zzh23@naver.com>
2023-07-15 21:02:48 +0900

    세 친구-시간 개선

--- 완전 탐색/세 친구.py
+++ 완전 탐색/세 친구.py
@@ -1,8 +1,11 @@
 from itertools import combinations
+import sys
+
+input = sys.stdin.readline
 
 # 친구 정보 세팅
 N, M = map(int, input().split())
-friends_num_list = [[] for idx in range(N)]
+friends_num_list = [[] for _ in range(N)]
 
 for _ in range(M):
     num1, num2 = map(int, input().split())
@@ -11,25 +14,18 @@ for _ in range(M):
 
 num_min = 4000
 
-for me_idx, friends_list in enumerate(friends_num_list):
-    check0 = True
-
-    # 각 경우에서 셋이 모두 친구인 경우 True 반환하기
-    for j in range(len(friends_list)-1):    
-        check1 = me_idx in friends_num_list[friends_list[j]]
-        for k in range(j+1, len(friends_list)):
-            check2 =  me_idx in friends_num_list[friends_list[k]]
-            check3 = friends_list[j] in friends_num_list[friends_list[k]] and friends_list[k] in friends_num_list[friends_list[j]]
-            
-            check0 = check1 and check2 and check3
-
-            # 셋이 모두 친구라면, 해당 경우의 전체 친구 수 구하기 - 서로
-            if check0:       
-                num = len(friends_list)+len(friends_num_list[friends_list[j]])+len(friends_num_list[friends_list[k]])
+for friends_list in friends_num_list:
+    # 셋이 모두 친구인 경우 찾기
+    for j in friends_list:    
+        for k in friends_num_list[j]:
+            if k in friends_list:
+                # 셋이 모두 친구라면, 해당 경우의 전체 친구 수 구하기 - 서로
+                num = len(friends_list)+len(friends_num_list[j])+len(friends_num_list[k])
                 num -= 2*3
                 if num < num_min:
                     num_min = num    
-
+            
+            
 if num_min == 4000:
     print(-1)
 else: print(num_min)
