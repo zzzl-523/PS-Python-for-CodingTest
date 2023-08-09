586635059aeff11b3a495be744123d2f64f08a89
Jihyun Lee <zzh23@naver.com>
2023-08-09 23:52:58 +0900

    선택정렬-selection sort

--- 정렬/기본 정렬 코드/선택정렬.py
+++ 정렬/기본 정렬 코드/선택정렬.py
@@ -1,12 +1,10 @@
-# 자기보다 앞에 정렬을 (큰것부터) 돌면서 자기보다 큰 값이 나오면 그 앞 값과 swap
-
 array = [7, 5, 9, 0, 3, 1, 6, 2, 4, 8]
 
-for i in range(1, len(array)):
-	for j in range(i, 0, -1):
-		if array[j] < array[j-1]:
-			array[j-1], array[j] = array[j], array[j-1] # 서로 바꿔준다
-		else:
-			break
+for i in range(len(array)):
+	min_index = i
+	for j in range(i+1, len(array)):
+		if array[j] < array[min_index]:
+			min_index = j
+	array[i], array[min_index] = array[min_index], array[i]
 
 print(array)
