5e8be4f20805166614a61b8ffa369f57cf966d24
Jihyun Lee <zzh23@naver.com>
2023-08-10 23:26:04 +0900

    퀵정렬

--- 정렬/기본 정렬 코드/퀵정렬.py
+++ 정렬/기본 정렬 코드/퀵정렬.py
@@ -1,27 +1,28 @@
 array = [5, 7, 9, 0, 3, 1, 6, 2, 4, 8]
 
 def quick_sort(array, start, end):
-    if start >= end:
+    if start >= end: # 원소가 1개인 경우 종료
         return
     pivot = start
     left = start + 1
     right = end
 
     while left <= right:
-      
+        # 피벗보다 큰 데이터를 찾을 때까지 반복 (왼쪽부터~)
         while left <= end and array[left] <= array[pivot]:
             left += 1
+        # 피벗보다 작은 데이터를 찾을 때까지 반복 (오른쪽부터~)
         while right > start and array[right] >= array[pivot]:
             right -= 1
         
-        if left > right:
+        if left > right: # 엇갈렸다면, 작은 데이터와 피벗을 교체
             array[pivot], array[right] = array[right], array[pivot]
-        else: array[left], array[right] = array[right], array[left]
+        else: # 엇갈리지 않았다면, 서로 교체
+            array[left], array[right] = array[right], array[left]
     
+    # 분할 이후 왼쪽 부분과 오른쪽 부분에서 각각 정렬 수행
     quick_sort(array, start, right-1)
     quick_sort(array, right+1, end)
-
-    # print(array)
 
 quick_sort(array, 0, len(array)-1)
 print(array)
