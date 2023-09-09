38c2a33ade711e705ef9cde16346aad5b62ee770
Jihyun Lee <zzh23@naver.com>
2023-09-09 23:53:50 +0900

    여행경로-런타임에러

--- 프로그래머스/여행경로.py
+++ 프로그래머스/여행경로.py
@@ -1,15 +1,27 @@
-tickets = [["ICN", "JFK"], ["HND", "IAD"], ["JFK", "HND"]]
+# 주어진 항공권 모두 사용이 함정!!!
+# 실패 케이스도 고려해야함!
 
-departure = []*len(tickets)
-arrival = []*len(tickets)
-for i in range(len(tickets)):
-    departure[i] = tickets[i][0]
-    arrival[i] = tickets[i][1]
+tickets = [["ICN", "SFO"], ["ICN", "ATL"], ["SFO", "ATL"], ["ATL", "ICN"], ["ATL","SFO"]]
 
-start = departure.index('ICN')
+tickets.sort(key=lambda x: (x[0], x[1]))
 
-answer = [start]
+departures = []
+arrivals = []
+
+for ticket in tickets:
+    departures.append(ticket[0])
+    arrivals.append(ticket[1])
+
+end = "ICN"
+answer = [end]
 
 for _ in range(len(tickets)):
-end = arrival[start]
-answer.append(end)
+    start_idx = departures.index(end)
+    departures.remove(end)
+
+    end = arrivals[start_idx]
+    arrivals.remove(end)
+    
+    answer.append(end)
+
+print(answer)
