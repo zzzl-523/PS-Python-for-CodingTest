15a8c39fb3873992490c411d9f8f46467b0fad72
Jihyun Lee <zzh23@naver.com>
2023-09-11 18:18:53 +0900

    상어초등학교

--- 프로그래머스/여행경로.py
+++ 프로그래머스/여행경로.py
@@ -1,27 +1,55 @@
-# 주어진 항공권 모두 사용이 함정!!!
-# 실패 케이스도 고려해야함!
+# DFS ..
 
+
+# for문 계속..10000개
+# 10000*10000 ..?
 tickets = [["ICN", "SFO"], ["ICN", "ATL"], ["SFO", "ATL"], ["ATL", "ICN"], ["ATL","SFO"]]
 
-tickets.sort(key=lambda x: (x[0], x[1]))
+def DFS(this, tickets, route, visited):
+    departure = this[0]
+    arrival = this[1]
+    print("루트 ", route)
 
-departures = []
-arrivals = []
+    if len(route) == len(tickets)+1:
+        return route
 
-for ticket in tickets:
-    departures.append(ticket[0])
-    arrivals.append(ticket[1])
 
-end = "ICN"
-answer = [end]
-
-for _ in range(len(tickets)):
-    start_idx = departures.index(end)
-    departures.remove(end)
-
-    end = arrivals[start_idx]
-    arrivals.remove(end)
+    for idx, ticket in enumerate(tickets):     
+        if ticket[0] == arrival and not visited[idx]:
+            print(this, ticket)
+            visited[idx] = True
+            route = DFS(ticket, tickets, route+[ticket[1]], visited)
     
-    answer.append(end)
+    print("뭐야?: ", route)
+    return route
 
-print(answer)
+
+def solution(tickets):
+    answer = []
+
+    for idx, ticket in enumerate(tickets):
+        if ticket[0] == "ICN":
+            route = ["ICN", ticket[1]]
+            visited = [False]*len(tickets)
+            visited[idx] = True
+            # result = []
+            answer.append(DFS(ticket, tickets, route, visited))
+            # print("--------------", ticket)
+    
+    answer.sort()
+    print(answer[0])
+    # print(answer[0])
+    return answer[0]
+
+print(solution([["ICN", "SFO"], ["ICN", "ATL"], ["SFO", "ATL"], ["ATL", "ICN"], ["ATL","SFO"]])==["ICN", "ATL", "ICN", "SFO", "ATL", "SFO"])
+print(solution([["ICN", "AOO"], ["AOO", "BOO"], ["BOO", "COO"], ["COO", "DOO"], ["DOO", "EOO"], ["EOO", "DOO"], ["DOO", "COO"], ["COO", "BOO"], ["BOO", "AOO"]]) == ["ICN", "AOO", "BOO", "COO", "DOO", "EOO", "DOO", "COO", "BOO", "AOO"])
+print(solution([["ICN", "AOO"], ["AOO", "BOO"], ["AOO", "COO"], ["COO", "AOO"], ["BOO", "ZOO"]]) == ["ICN", "AOO", "COO", "AOO", "BOO", "ZOO"])
+print(solution([["ICN", "AOO"], ["AOO", "BOO"], ["AOO", "BOO"], ["BOO", "AOO"], ["BOO", "FOO"], ["FOO", "COO"], ["COO", "ZOO"]]) == ["ICN", "AOO", "BOO", "AOO", "BOO", "FOO", "COO", "ZOO"])
+print(solution([["ICN", "BOO"], ["ICN", "COO"], ["COO", "DOO"], ["DOO", "COO"], ["BOO", "DOO"], ["DOO", "BOO"], ["BOO", "ICN"], ["COO", "BOO"]]) == ["ICN", "BOO", "DOO", "BOO", "ICN", "COO", "DOO", "COO", "BOO"])
+print(solution([["ICN", "AAA"], ["ICN", "AAA"], ["ICN", "AAA"], ["AAA", "ICN"], ["AAA", "ICN"]]) == ["ICN", "AAA", "ICN", "AAA", "ICN", "AAA"])
+print(solution([["ICN", "BBB"],["ICN", "CCC"],["BBB", "CCC"],["CCC", "BBB"],["CCC", "ICN"]]) == ["ICN", "BBB", "CCC", "ICN", "CCC", "BBB"])
+print(solution([["ICN", "JFK"], ["HND", "IAD"], ["JFK", "HND"]]) == ["ICN", "JFK", "HND", "IAD"])
+print(solution([["ICN", "SFO"], ["ICN", "ATL"], ["SFO", "ATL"], ["ATL", "ICN"], ["ATL","SFO"]]) == ["ICN", "ATL", "ICN", "SFO", "ATL", "SFO"])
+print(solution([["ICN", "AOO"], ["ICN", "AOO"], ["AOO", "ICN"], ["AOO", "COO"]]) == ["ICN", "AOO", "ICN", "AOO", "COO"])
+print(solution([["ICN", "AAA"], ["ICN", "AAA"], ["ICN", "AAA"], ["AAA", "ICN"], ["AAA", "ICN"]]) == ["ICN", "AAA", "ICN", "AAA", "ICN", "AAA"])
+print(solution([["ICN", "BOO"], ["ICN", "COO"], ["COO", "DOO"], ["DOO", "COO"], ["BOO", "DOO"], ["DOO", "BOO"], ["BOO", "ICN"], ["COO", "BOO"]]) == ["ICN", "BOO", "DOO", "BOO", "ICN", "COO", "DOO", "COO", "BOO"])
