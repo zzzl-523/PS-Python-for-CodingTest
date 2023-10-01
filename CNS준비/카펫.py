a8eab9c83b935fcee3ed8ced680e848e623772cb
Jihyun Lee <zzh23@naver.com>
2023-10-01 22:24:22 +0900

    short ver

--- CNS준비/카펫.py
+++ CNS준비/카펫.py
@@ -8,29 +8,46 @@
 # 약수 구하기
 # => 노랑이에서 구해서 사각형 만들고, 거기서 가로,세로2씩 커지면 됨
 
-import math
 
-brown = 24
-yellow = 24
+import math
 
 def solution(brown, yellow):
     arr = []
-    garo, sero = 0, 0
     for i in range(1, int(math.sqrt(yellow))+1):
         if yellow%i==0:
-            sero = i
-            garo = yellow//i
             total = brown + yellow
-            if total%(sero+2)==0 and total%(garo+2)==0:
+            if total%(i+2)==0 and total%(yellow//i+2)==0:
                 arr.append((i, yellow//i))
+
+    answer = [arr[-1][1] + 2, arr[-1][0] + 2]
     
-    sero = arr[-1][0] + 2
-    garo = arr[-1][1] + 2
-    answer = [garo, sero]
-    
-    print(answer)
     return(answer)
+
+
+
+# import math
+
+# brown = 24
+# yellow = 24
+
+# def solution(brown, yellow):
+#     arr = []
+#     garo, sero = 0, 0
+#     for i in range(1, int(math.sqrt(yellow))+1):
+#         if yellow%i==0:
+#             sero = i
+#             garo = yellow//i
+#             total = brown + yellow
+#             if total%(sero+2)==0 and total%(garo+2)==0:
+#                 arr.append((i, yellow//i))
     
-solution(brown, yellow)
+#     sero = arr[-1][0] + 2
+#     garo = arr[-1][1] + 2
+#     answer = [garo, sero]
+    
+#     print(answer)
+#     return(answer)
+    
+# solution(brown, yellow)
     
 
