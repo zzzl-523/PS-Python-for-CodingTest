680a64c08a407bc7eb3038f9dc85507d29e5709b
Jihyun Lee <zzh23@naver.com>
2023-10-01 20:15:19 +0900

    돌 굴러가유 - 통과

--- 삼성sw기출/돌굴러가유.py
+++ 삼성sw기출/돌굴러가유.py
@@ -7,6 +7,8 @@
 # 돌이 나오는 위치로 잡고, 최소가 되도록 구하기
 # 돌의 개수에서 벽의 개수로 조합을 구해야 하는데, 이걸 다 구하면 25*24/2 => 300
 
+# 돌과 벽에 의해 영향 받는 구역을 벽 개수로 나눌 수 있음 -> 영향 구역 수는 돌 개수와 같음
+
 import sys
 from itertools import combinations
 input = sys.stdin.readline
@@ -15,39 +17,17 @@ N, M, K = map(int, input().split())
 morae_list = list(map(int, input().split()))
 stones = list(map(int, input().split()))
 
-# 벽의 개수 만큼 뽑기 -> 조합 
-combs = list(combinations(stones, M))
-min_value = 100000000
-result = []
-states = [0]*N
+# 돌 시작되는 위치로 구역 나누기
+arr = [(-1, -1)]*K # 마을 번호, 부서지는 모래성 수
+for idx in range(len(stones)):
+    if idx == len(stones)-1:
+        arr[idx] = (stones[idx], sum(morae_list[stones[idx]-1:]))
+    else: arr[idx] = (stones[idx], sum(morae_list[stones[idx]-1:stones[idx+1]-1]))
 
-# 돌 굴러오는 위치 표시
-for idx in stones:
-    states[idx-1] = -1
+# 피해 모래성 최소로 하는 벽 구하기
+arr.sort(key=lambda x: (-x[1], x[0]))
+arr = arr[:M]
+arr.sort(key=lambda x: x[0])
 
-# 벽 위치 표시
-for combi in combs:
-    temp_states = [num for num in states]
-
-    for j in range(len(combi)):
-        temp_states[combi[j]-1] = 1
-
-    total = 0
-    check = True
-    for i in range(N):
-        if temp_states[i] == -1:
-            check = True
-        elif temp_states[i] == 1:
-            check = False
-        
-        if check:
-            total += morae_list[i]
-    
-    if total < min_value:
-        min_value = total
-        result = [combi]
-    elif total == min_value:
-        result.append(combi)
-
-result.sort()
-print(*result[0], sep='\n')
+for num in arr:
+    print(num[0])
