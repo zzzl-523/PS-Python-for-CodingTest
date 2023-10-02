a34545f32f5caa552990c68907f4b5f5af6c3e14
Jihyun Lee <zzh23@naver.com>
2023-10-02 22:26:36 +0900

    RGB거리2

--- CNS준비/RGB거리2.py
+++ CNS준비/RGB거리2.py
@@ -2,26 +2,24 @@ import sys
 input = sys.stdin.readline
 
 N = int(input())
-DP = []
-for i in range(N):
-    DP.append(list(map(int, input().split())))
+RGB = []
+INF = 2148000000
+ans = INF
+for _ in range(N):
+    RGB.append(list(map(int, input().split())))
 
-for i in range(1, N-1):
-    for j in range(3):
-        if j == 0:
-            DP[i][j] += min(DP[i-1][1], DP[i-1][2])
-        elif j == 1:
-            DP[i][j] += min(DP[i-1][0], DP[i-1][2])
-        elif j == 2:
-            DP[i][j] += min(DP[i-1][0], DP[i-1][1])
+for k in range(3):
+    # 1번째 비교
+    DP = [[INF, INF, INF] for _ in range(N)]
+    DP[0][k] = RGB[0][k]
 
+    for i in range(1, N):
+        DP[i][0] = RGB[i][0] + min(DP[i-1][1], DP[i-1][2])
+        DP[i][1] = RGB[i][1] + min(DP[i-1][0], DP[i-1][2])
+        DP[i][2] = RGB[i][2] + min(DP[i-1][0], DP[i-1][1])
 
-for i in range(3):
-    if i == 0:
-        DP[N-1][i] += min(DP[(N-1)-1][1], DP[(N-1)-1][2])
-    elif i == 1:
-        DP[N-1][i] += min(DP[(N-1)-1][0], DP[(N-1)-1][2])
-    elif i == 2:
-        DP[N-1][i] += min(DP[(N-1)-1][0], DP[(N-1)-1][1])
+    for i in range(3):
+        if i != k:
+            ans = min(ans, DP[-1][i])
 
-print(min(DP[-1]))
+print(ans)
