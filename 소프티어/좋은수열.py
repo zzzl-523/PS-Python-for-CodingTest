36e8ccacc104434ec2746c0fb4257e3e6683d5dc
Jihyun Lee <zzh23@naver.com>
2023-11-08 23:57:37 +0900

    좋은수열-백트래킹 구현

--- 소프티어/좋은수열.py
+++ 소프티어/좋은수열.py
@@ -1,20 +1,41 @@
-def check(num):
-    length = len(num)
-    for idx in range(1, length//2 + 1):
-        if num[-idx:] == num[-(idx*2):-idx]:
-            return False
-    else:
-        return True
+# 백트래킹
+# 배열 만들면서 바로 뒤에 이어지는지 나쁜수열 확인
+# 좋은수열일 경우, 저장.
 
-def recursive(num):
-    global N, res
-    if len(num) == N:
-        print(num)
-        exit()
-    for i in '123':
-        if check(num + str(i)):
-            recursive(num + str(i))
-    return
+# 확인할 때는 같은 숫자 연속 안 되도록 생성 / 만들면서 확인
 
+import copy
 N = int(input())
-recursive('1')
+ans = 10*81
+
+def check(arr):
+    for i in range(2,(len(arr)//2)+1):
+        if arr[len(arr) - i:] == arr[len(arr) - i*2 :  len(arr) - i ]:
+            return False
+    
+    return True
+        
+
+def dfs(arr, num):
+    global ans
+
+    if not check(arr):
+        return
+    if len(arr)==N:
+        ans = min(ans, int(''.join(arr)))
+    
+    if num == 1:
+        dfs(copy.deepcopy(arr)+[2], 2)
+        dfs(copy.deepcopy(arr)+[3], 3)
+    elif num == 2:
+        dfs(copy.deepcopy(arr)+[1], 1)
+        dfs(copy.deepcopy(arr)+[3], 3)
+    elif num == 3:
+        dfs(copy.deepcopy(arr)+[2], 2)
+        dfs(copy.deepcopy(arr)+[1], 1)
+
+ 
+for i in range(3):
+    dfs([i], i)
+
+print(ans)
