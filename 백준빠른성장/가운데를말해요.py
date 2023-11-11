0c1370e7b3ede15aa666dc3f106b72e6e8379d0e
Jihyun Lee <zzh23@naver.com>
2023-11-11 23:52:41 +0900

    개선

--- 백준빠른성장/가운데를말해요.py
+++ 백준빠른성장/가운데를말해요.py
@@ -1,18 +1,42 @@
-# 단기간 성장
-# 홀수면 전체 수 중에서 중간값
-# 짝수면 중간 두 수 중에서 작은 수
+# # 단기간 성장
+# # 홀수면 전체 수 중에서 중간값
+# # 짝수면 중간 두 수 중에서 작은 수
 
-N = int(input())
+# N = int(input())
 
-arr = [-1]
+# arr = [-1]
 
-for i in range(1, N+1):
-    arr.append(int(input()))
+# for i in range(1, N+1):
+#     arr.append(int(input()))
 
-    # 짝수
-    if i%2==0:
-        print(min(arr[len(arr)//2], arr[(len(arr)//2)+1]))
+#     # 짝수
+#     if i%2==0:
+#         print(min(arr[len(arr)//2], arr[(len(arr)//2)+1]))
+#     else:
+#         print(sorted(arr)[len(arr)//2])
+
+# # sort 방법 바꿔보기!  
+
+import heapq
+import sys
+
+n = int(sys.stdin.readline())
+
+leftHeap = []
+rightHeap = []
+for i in range(n):
+    num = int(sys.stdin.readline())
+
+    if len(leftHeap) == len(rightHeap):
+        heapq.heappush(leftHeap, -num)
     else:
-        print(sorted(arr)[len(arr)//2])
+        heapq.heappush(rightHeap, num)
 
-# sort 방법 바꿔보기!  
+    if rightHeap and rightHeap[0] < -leftHeap[0]:
+        leftValue = heapq.heappop(leftHeap)
+        rightValue = heapq.heappop(rightHeap)
+
+        heapq.heappush(leftHeap, -rightValue)
+        heapq.heappush(rightHeap, -leftValue)
+
+    print(-leftHeap[0])
