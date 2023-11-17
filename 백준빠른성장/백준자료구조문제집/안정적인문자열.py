5d069aa67e5a434ac007ada5cf2ebdec581ced13
Jihyun Lee <zzh23@naver.com>
2023-11-18 01:45:09 +0900

    안정적인문자열-해결

--- 백준빠른성장/백준자료구조문제집/안정적인문자열.py
+++ 백준빠른성장/백준자료구조문제집/안정적인문자열.py
@@ -3,10 +3,26 @@ input = sys.stdin.readline
 
 T = 0
 arr = []
-is_ok = False
+is_open = False
+
 while True:
     T += 1
+    ans = 0
     arr = input()
+    s = []
+    if '-' in arr:
+        break
     for char in arr:
-        if is_ok and char=='{':
-            
+        if char=='{':
+            s.append(char)
+        elif char=='}':
+            if len(s)==0:
+                ans += 1
+                s.append('{')
+            else:
+                s.pop()
+
+    if len(s)>0:
+        ans += len(s)/2
+    
+    print(str(T)+'.',int(ans))
